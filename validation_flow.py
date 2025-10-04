e27e6a583a77fa2de74cd0d9adb2acd8846918ce
Naveen Kumar <naveenkumargontyala@gmail.com>
2025-10-04 10:49:28 +0530

    ml_flow_complete

---
+++ validation_flow.py
@@ -0,0 +1,24 @@
+
+from prefect import flow, task
+import pandas as pd
+@task
+def ingest_data():
+    df = pd.DataFrame({
+        "age": [25, 32, 20, 40],
+        "bp": [120, 130, 112, 140]
+    })
+    return df
+@task
+def validate_data(df: pd.DataFrame):
+    if (df["age"] <= 0).any():
+        raise ValueError("❌ Invalid age detected")
+    if df.isnull().any().any():
+        raise ValueError("❌ Missing values detected")
+    print("✅ Data validation passed")
+    return df
+@flow
+def validation_pipeline():
+    df = ingest_data()
+    validate_data(df)
+if __name__ == "__main__":
+    validation_pipeline()
